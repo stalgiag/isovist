6f5fd224d3ca287e73dd9beb137db98db6da1dcd
Stalgia Grigg <stalgia.grigg@gmail.com>
2024-11-08 17:07:41 -0800

    Project view

--- src/components/ProjectView.tsx
+++ src/components/ProjectView.tsx
@@ -1,5 +1,6 @@
 import { Image } from "astro:assets";
 import { useState, useEffect } from "preact/hooks";
+import bracketImage from "../assets/brackets.png";
 
 export default function ProjectView({ projects }) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -20,11 +21,16 @@ export default function ProjectView({ projects }) {
         </h2>
         <p class="text-foreground-secondary">{projects[currentIndex].body}</p>
       </div>
-      <div class="w-full h-[200px] mix-blend-screen flex justify-center items-center">
+      <div class="w-full h-[200px] flex justify-center items-center self-center relative">
         <img
-          class="w-auto h-full object-contain"
+          class="w-auto h-full object-contain relative z-10 mix-blend-screen"
           src={projects[currentIndex].data.featuredImage.src}
           alt={projects[currentIndex].data.featuredImageAlt}
+        />
+        <img
+          class="w-auto h-[300px] object-contain absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          src={bracketImage.src}
+          alt="bracket"
         />
       </div>
     </>
